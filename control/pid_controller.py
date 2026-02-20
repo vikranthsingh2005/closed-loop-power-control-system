055183c643157ab2d4c1d8728565bffad463ea40
Vikranth <167434977+vikranthsingh2005@users.noreply.github.com>
2026-02-20 12:08:09 -0800

    Implement PID control logic

--- control/pid_controller.py
+++ control/pid_controller.py
@@ -1,1 +1,23 @@
+class PIDController:
+    def __init__(self, Kp, Ki, Kd, setpoint):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.setpoint = setpoint
 
+        self.integral = 0
+        self.prev_error = 0
+
+    def compute(self, measured_value, dt):
+        error = self.setpoint - measured_value
+        self.integral += error * dt
+        derivative = (error - self.prev_error) / dt
+
+        output = (
+            self.Kp * error +
+            self.Ki * self.integral +
+            self.Kd * derivative
+        )
+
+        self.prev_error = error
+        return output
