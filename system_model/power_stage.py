36ff4c56b9b64062bef26a8b58988bfa753a27ae
Vikranth <167434977+vikranthsingh2005@users.noreply.github.com>
2026-02-20 12:07:14 -0800

    Implement first-order DC power stage dynamic model

--- system_model/power_stage.py
+++ system_model/power_stage.py
@@ -1,1 +1,17 @@
+import numpy as np
 
+class DCPowerStage:
+    """
+    First-order DC power stage model:
+    dV/dt = (1/RC)(Vin - Vout)
+    """
+
+    def __init__(self, R=1.0, C=0.01):
+        self.R = R
+        self.C = C
+        self.Vout = 0.0
+
+    def step(self, Vin, dt):
+        dVdt = (1 / (self.R * self.C)) * (Vin - self.Vout)
+        self.Vout += dVdt * dt
+        return self.Vout
